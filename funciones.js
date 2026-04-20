98bfcb4106861c87bf978069839ed7b55154d2b8
OskarPaul93 <paul_1993oscar@hotmail.com>
2026-04-19 19:12:23 -0500

    Disponible= Ingresos - Egresos

--- funciones.js
+++ funciones.js
@@ -1,1 +1,28 @@
+function recuperarTexto(idComponente){
+    let componente= document.getElementById(idComponente);
+    let valor= componente.value;
+
+    return valor;
+}
+
+function recuperarFloat (idComponente){
+    let valorTexto= recuperarTexto(idComponente);
+    let valorFloat= parseFloat (valorTexto);
+
+    return valorFloat
+}
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+function calcularDisponible (ingresos, egresos){
+    let disponible= ingresos - egresos;
+    disponible= disponible.toFixed(2);
+    if (disponible<0){
+        disponible= 0;
+
+    }
+
+
+
+    return disponible; 
+}
+
+
--- simulador.js
+++ simulador.js
@@ -1,1 +1,21 @@
+
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+function calcular(){
+    //let ingresos= parsefloat(document.getElementById("txtIngresos").value);
+    //let egresos= parsefloat(document.getElementById("txtEgresos").value);
+
+    //let disponible= calcularDisponible (ingresos, egresos);
+    let ingresos= recuperarFloat("txtIngresos");
+    let egresos= recuperarFloat("txtEgresos");
+
+    //Guardar retorno en una variable
+    let disponible= calcularDisponible (ingresos, egresos);
+    
+
+    //Paso previo para mostrar la respuesta
+    let spDisponible= document.getElementById("spnDisponible");
+
+    //Mostrar texto en pantalla
+    spDisponible.textContent= disponible;
+
+}
